3648d49ce01eac4553f8738dfe91accee7592136
Deekshant Yadav <deekshant@imgglobalinfotech.com>
2021-11-17 14:47:12 +0530

    7.0.2

---
+++ app/src/main/java/com/img/sdkapplication/HelloDebug.java
@@ -0,0 +1,18 @@
+package com.img.sdkapplication;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+public class HelloDebug {
+    public static final String TAG = "SUPER_AWESOME_APP";
+
+    public static void d(String message){
+        Log.d( TAG, message);
+    }
+    public static void ToastShow(Context context,String message){
+        Log.d( TAG, message);
+        Toast.makeText(context, message+"", Toast.LENGTH_SHORT).show();
+    }
+
+}
